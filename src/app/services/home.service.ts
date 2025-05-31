4537d6b5853af7a4566e8246799a76fb1ac7acab
Theerakarn-Kangonta <theerakarn.kangonta@gmail.com>
2025-05-31 20:55:08 +0700

    update home

--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Product } from '../models/product';
 import { environment } from '../../environment/environment';
 
 @Injectable({
